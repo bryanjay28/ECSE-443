fd0a689b60dfd0601bb915c727c08b2b00904068
bryanjay28 <bjay28@hotmail.com>
2019-01-27 13:14:30 -0500

    assign 1 base

---
+++ assignment1/main.m
@@ -0,0 +1,7 @@
+% Q1 Answers
+x_q1 = [10, 1000, 1000000];
+y_q1 = [];
+
+for i = 1:length(x_q1)
+   y_q1.append(q1_eqn(x_q1(i)));
+end
---
+++ assignment1/q1_eqn.m
@@ -0,0 +1,5 @@
+% function from Q1
+function f_x = q1_eqn(x)
+
+f_x = x*(sqrt(x)*sqrt(x-1));
+end
